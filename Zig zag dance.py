6a475034fd6829941bb042459bb6ec03f08a2b24
Rudra shah <51542683+scienceLabwork@users.noreply.github.com>
2019-10-24 12:30:26 +0530

    Add files via upload

---
+++ Zig zag dance.py
@@ -0,0 +1,8 @@
+# 24/10/19 RUDRA's CODE!
+
+x = '*'
+for k in range(0,2000000000000000000000000000000):
+    for i in range(0,8):
+        print(' '*i,x+x,'      ',' '*i,x+x)
+    for j in range(7,0,-1):
+        print(' '*j,x+x,'      ',' '*j,x+x)
